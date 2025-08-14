99e5fe564692561d91ef9ed2bf332e5e616e5249
quangtranphu <quangtranphu0902@gmail.com>
2025-08-14 11:06:05 +0200

    first commit

--- main.py
+++ main.py
@@ -5,28 +5,17 @@ import pandas as pd
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from loguru import logger
-from pydantic import BaseModel
+from schemas import *
 
 # Creating FastAPI instance
 app = FastAPI()
 # Creating class to define the request body
 # and the type hints of each attribute
-class HouseInfo(BaseModel):
-    MSSubClass: int = 60
-    MSZoning: str = "RL"
-    LotArea: int = 7844
-    LotConfig: str = "Inside"
-    BldgType: str = "1Fam"
-    OverallCond: int = 7
-    YearBuilt: int = 1978
-    YearRemodAdd: int = 1978
-    Exterior1st: str = "HdBoard"
-    BsmtFinSF2: float = 0.0
-    TotalBsmtSF: float = 672.0
+
 
 
 # Loading model with default path models/model.pkl
-clf = joblib.load(os.environ.get("MODEL_PATH", "models/model.pkl"))
+clf = joblib.load("./models/model.pkl")
 
 # Creating an endpoint to receive the data
 # to make prediction on.
